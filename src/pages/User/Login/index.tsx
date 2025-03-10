0245265743f07a075a5a5edd22b0e071f8030b20
DianaLeoTang <wangyaotang0228@gmail.com>
2025-03-10 11:35:42 +0800

    feat: 修改登录提示

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -225,7 +225,7 @@ const Login: React.FC = () => {
                 }}
                 placeholder={intl.formatMessage({
                   id: 'pages.login.username.placeholder',
-                  defaultMessage: '用户名: admin or user',
+                  defaultMessage: '用户名: user',
                 })}
                 rules={[
                   {
@@ -247,7 +247,7 @@ const Login: React.FC = () => {
                 }}
                 placeholder={intl.formatMessage({
                   id: 'pages.login.password.placeholder',
-                  defaultMessage: '密码: ant.design',
+                  defaultMessage: '密码: 666',
                 })}
                 rules={[
                   {
